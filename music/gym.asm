d2b6a68f1f514c0e383aa69a9bcee117e54d56ac
U-Fish-PC\Daniel <corrnondacqb@yahoo.com>
2013-11-11 19:47:04 -0500

    Add GSC Gym theme

--- music/gym.asm
+++ music/gym.asm
@@ -1,358 +1,496 @@
-Music_Gym_Ch1: ; bcbb (2:7cbb)
-	tempo 0, 138
+Music_Gym_Ch1:
+	tempo 0, 156
 	stereopanning 119
-	duty 3
-	vibrato 8, 2, 2
-	togglecall
-	notetype 12, 11, 5
-	octave 3
-	note G_, 6
-	note C_, 1
-	note G_, 1
-	note F_, 6
-	octave 2
-	note A#, 1
-	octave 3
-	note F_, 1
-	note E_, 6
+	vibrato 18, 2, 4
+	duty 1
+	notetype 12, 11, 7
+
+Music_Gym_Ch1_branch_1:
+	octave 4
+	note F#, 2
 	octave 2
 	note A_, 1
-	octave 3
-	note E_, 1
-	note F_, 4
-	note G_, 4
-
-Music_Gym_branch_bcd8:
-	note E_, 4
-	note F_, 2
-	note G_, 2
-	note F_, 2
-	note E_, 2
-	note D_, 2
-	note C_, 2
-	notetype 12, 11, 1
-	note D_, 3
-	notetype 12, 11, 5
-	note E_, 1
-	note F_, 4
-	note E_, 2
-	note D_, 2
-	note E_, 2
-	note F_, 2
-	note E_, 4
-	note F_, 2
-	note G_, 2
-	note F_, 2
-	note E_, 2
-	note D_, 2
-	note C_, 2
-	note D_, 2
-	note D_, 1
-	note E_, 1
-	note F_, 4
-	note E_, 2
-	note D_, 2
-	note E_, 2
-	note F_, 2
+	note A_, 1
+	note A_, 1
+	rest 3
 	octave 4
-	note C_, 4
-	octave 3
-	note A#, 2
-	octave 4
-	note C_, 2
-	octave 3
-	note A#, 2
-	note A_, 2
-	note G_, 2
-	note F_, 2
-	notetype 12, 11, 1
-	note A#, 3
-	notetype 12, 11, 5
-	note F_, 1
-	note F_, 4
 	note E_, 2
-	note D_, 2
-	note E_, 2
-	note F_, 2
-	octave 4
-	note C_, 4
-	octave 3
-	note A#, 2
-	octave 4
-	note C_, 2
-	octave 3
-	note A#, 2
-	note A_, 2
-	note G_, 2
-	note F_, 2
-	note D_, 2
-	note D_, 1
-	note E_, 1
-	note F_, 4
-	note A#, 2
-	note F_, 1
-	note A#, 1
-	octave 4
-	note D_, 4
-	octave 3
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note G_, 2
-	note F_, 2
-	note E_, 2
-	note D_, 2
-	note E_, 2
-	note F_, 2
-	note D_, 2
-	note E_, 2
-	note C_, 2
-	note D_, 2
-	note E_, 2
-	note C_, 2
-	note D_, 2
-	note C_, 2
-	note C_, 1
-	note D_, 1
-	note E_, 4
-	note C_, 2
-	note E_, 2
-	note D_, 2
-	note C_, 2
-	note E_, 2
-	note C_, 1
-	note E_, 1
-	note F_, 4
-	note E_, 2
-	note D_, 2
-	note E_, 2
-	note F_, 2
-	note E_, 2
-	note C_, 2
-	note E_, 2
-	note G_, 2
-	note F_, 2
-	note E_, 2
-	note D_, 2
-	note E_, 2
-	note D_, 2
-	note F_, 2
-	note D_, 2
-	note E_, 2
-	note C_, 2
-	note D_, 2
-	note C_, 2
-	note D_, 2
-	note C_, 2
-	note C_, 1
-	note D_, 1
-	note E_, 4
-	note D_, 2
-	note E_, 2
-	note F_, 2
-	note F_, 2
-	note G_, 2
-	note E_, 1
+	octave 2
 	note G_, 1
-	note A#, 2
-	octave 4
-	note C_, 2
-	octave 3
-	note A#, 2
-	note A_, 2
-	note G_, 2
-	note A_, 2
-	loopchannel 0, Music_Gym_branch_bcd8
-
-
-Music_Gym_Ch2: ; bd6b (2:7d6b)
-	duty 3
-	vibrato 10, 2, 5
-	notetype 12, 12, 7
-	octave 4
-	note C_, 6
-	octave 3
 	note G_, 1
+	note G_, 1
+	rest 3
 	octave 4
-	note C_, 1
+	note D_, 2
+	octave 2
+	note D_, 1
+	note D_, 1
+	note D_, 1
+	rest 1
 	octave 3
-	note A#, 6
-	note F_, 1
-	note A#, 1
-	notetype 12, 10, 0
+	note D_, 1
+	note D_, 1
+	note B_, 1
+	note G_, 1
+	note D_, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note A_, 1
+	note E_, 1
+	octave 4
+	note E_, 1
+	octave 3
 	note A_, 12
-	notetype 12, 12, 7
-	note B_, 4
-
-Music_Gym_branch_bd82:
-	octave 4
-	note C_, 12
-	octave 3
-	note G_, 2
-	octave 4
-	note C_, 2
-	notetype 12, 12, 2
-	note D_, 3
-	octave 3
-	note A#, 1
-	notetype 12, 12, 7
-	note A#, 12
-	octave 4
-	note C_, 12
-	octave 3
-	note G_, 2
-	octave 4
-	note C_, 2
-	notetype 12, 12, 4
-	octave 3
-	note A#, 2
-	notetype 12, 12, 7
-	note A#, 1
-	octave 4
-	note C_, 1
-	note D_, 12
-	note E_, 12
-	note D_, 2
-	note E_, 2
-	notetype 12, 12, 2
-	note F_, 3
-	note D_, 1
-	notetype 12, 12, 7
-	note D_, 4
-	notetype 12, 12, 2
-	note A#, 3
-	note F_, 1
-	notetype 12, 12, 7
-	note F_, 4
-	note E_, 12
-	note D_, 2
-	note E_, 2
-	note F_, 2
-	note D_, 1
-	note F_, 1
-	note A#, 12
-	octave 3
-	note G_, 6
-	octave 4
-	note C_, 6
-	octave 3
-	note G_, 4
-	note A#, 4
-	note A_, 4
-	note G_, 4
-	note F_, 4
-	notetype 12, 12, 3
-	note E_, 2
-	notetype 12, 12, 7
-	note E_, 1
-	note F_, 1
-	note G_, 6
-	note G_, 2
-	octave 4
-	note C_, 2
-	octave 3
-	note G_, 2
-	note A#, 4
-	note A_, 4
-	note G_, 4
-	note C_, 2
-	note D_, 2
-	note G_, 6
-	octave 4
-	note C_, 6
-	octave 3
-	note G_, 4
-	note A#, 4
-	note A_, 4
-	note G_, 4
-	note F_, 4
-	notetype 12, 12, 3
-	note E_, 2
-	notetype 12, 12, 7
-	note E_, 1
-	note F_, 1
-	note G_, 4
-	notetype 12, 12, 3
-	note G_, 2
-	notetype 12, 12, 7
-	note G_, 1
-	note A_, 1
-	note A#, 4
-	notetype 12, 12, 4
-	octave 4
-	note C_, 2
-	notetype 12, 12, 7
-	note C_, 1
-	note D_, 1
-	note E_, 12
-	loopchannel 0, Music_Gym_branch_bd82
-
-
-Music_Gym_Ch3: ; bdfa (2:7dfa)
-	notetype 12, 1, 1
-	rest 16
-	rest 10
-	octave 4
-	note G_, 2
-	note F_, 2
-	note D_, 2
-
-Music_Gym_branch_be02:
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be44
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be44
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be44
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be44
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be4d
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be4d
-	callchannel Music_Gym_branch_be3b
-	callchannel Music_Gym_branch_be4d
-	callchannel Music_Gym_branch_be3b
-	note F_, 2
-	note A#, 2
-	note F_, 2
-	note A#, 2
-	note F_, 2
-	note A#, 2
+	note F#, 2
 	note A_, 2
-	note F_, 2
-	loopchannel 0, Music_Gym_branch_be02
+	note G_, 1
+	rest 2
+	note E_, 1
+	note E_, 2
+	note C_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note G_, 2
+	note F#, 8
+	note A_, 4
+	note F#, 2
+	note A_, 2
+	note E_, 1
+	rest 1
+	note E_, 1
+	note F#, 1
+	note G_, 4
+	octave 4
+	note C_, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	octave 4
+	note C_, 2
+	note D_, 6
+	octave 3
+	note A_, 2
+	note F#, 2
+	note E_, 2
+	note G_, 2
+	note A_, 2
+	note B_, 1
+	rest 2
+	note G_, 1
+	note G_, 4
+	octave 4
+	note E_, 1
+	rest 2
+	note C_, 1
+	note C_, 2
+	octave 3
+	note B_, 1
+	note A#, 1
+	note A_, 12
+	note F#, 2
+	note A_, 2
+	octave 4
+	note C_, 2
+	octave 3
+	note G_, 1
+	octave 4
+	note C_, 1
+	note E_, 2
+	note C_, 2
+	note E_, 2
+	note C_, 1
+	note E_, 1
+	note G_, 4
+	note D_, 6
+	octave 3
+	note A_, 1
+	rest 3
+	note A_, 1
+	rest 1
+	note A_, 1
+	octave 4
+	note F#, 3
+	note G_, 4
+	note F#, 4
+	note E_, 4
+	note C_, 4
+	note D_, 1
+	rest 1
+	note D_, 1
+	note E_, 1
+	rest 2
+	octave 3
+	note B_, 4
+	note F#, 2
+	note D_, 2
+	note F#, 2
+	note A#, 1
+	note A_, 1
+	note G_, 1
+	note F_, 1
+	octave 4
+	note D_, 4
+	note C_, 1
+	octave 3
+	note A#, 1
+	note A_, 1
+	note G_, 1
+	octave 4
+	note E_, 4
+	note F#, 6
+	octave 3
+	note A_, 1
+	rest 3
+	note A_, 1
+	rest 1
+	note A_, 1
+	octave 4
+	note F#, 3
+	note A_, 4
+	note G_, 4
+	note F#, 4
+	note E_, 4
+	note D_, 1
+	octave 3
+	note B_, 1
+	note F#, 1
+	octave 4
+	note E_, 1
+	note F#, 4
+	note E_, 1
+	note C_, 1
+	octave 3
+	note G_, 1
+	octave 4
+	note F#, 1
+	note G_, 4
+	note A_, 1
+	note G_, 1
+	note F#, 1
+	note E_, 1
+	note A_, 4
+	octave 5
+	note D_, 4
+	octave 4
+	note A_, 1
+	note G_, 1
+	note F#, 1
+	note E_, 1
+	loopchannel 0, Music_Gym_Ch1_branch_1
 
-Music_Gym_branch_be3b:
+Music_Gym_Ch2:
+	duty 1
+
+Music_Gym_Ch2_branch_1:
+	notetype 12, 12, 3
+	octave 2
+	note D_, 2
+	octave 1
+	note D_, 1
+	note D_, 1
+	note D_, 2
+	note A_, 1
+	octave 2
+	note F#, 1
+	note C_, 2
+	octave 1
+	note C_, 1
+	note C_, 1
+	note C_, 2
+	octave 2
+	note G_, 1
+	note E_, 1
+	octave 1
+	note G_, 2
+	octave 1
+	note G_, 1
+	note G_, 1
+	note G_, 2
+	octave 1
+	note G_, 1
+	note G_, 1
+	note A_, 4
+	octave 2
+	note A_, 2
+	octave 3
+	note C_, 1
+	note C#, 1
+	callchannel Music_Gym_Ch2_branch_2
+	octave 3
 	note E_, 2
+	note C_, 1
+	note C#, 1
+	callchannel Music_Gym_Ch2_branch_2
+	octave 3
+	note C_, 1
+	note E_, 1
+	note G_, 1
+	octave 4
+	note C_, 1
+	notetype 12, 13, 2
+	callchannel Music_Gym_Ch2_branch_3
+	octave 1
+	note F#, 1
+	note B_, 1
+	note F#, 2
+	octave 2
+	note F#, 2
+	octave 1
+	note B_, 2
+	note A#, 4
+	note A#, 2
+	note F_, 1
+	note F_, 1
+	note G_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note G_, 2
+	octave 2
 	note G_, 2
 	note E_, 2
+	callchannel Music_Gym_Ch2_branch_3
+	octave 1
+	note G_, 1
+	octave 2
+	note C_, 1
+	octave 1
 	note G_, 2
+	octave 2
 	note E_, 2
-	note G_, 2
+	note C_, 2
+	note D_, 4
+	note D_, 2
+	octave 1
+	note A_, 1
+	note A_, 1
+	note A_, 1
+	octave 2
+	note D_, 1
+	octave 1
+	note A_, 2
+	octave 2
+	note F#, 2
+	note D_, 2
+	loopchannel 0, Music_Gym_Ch2_branch_1
+
+Music_Gym_Ch2_branch_2:
+	octave 3
+	note D_, 3
+	octave 2
+	note D_, 3
+	note D_, 2
+	note D_, 1
+	note D_, 1
+	note D_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note A_, 2
+	octave 3
+	note C_, 3
+	octave 2
+	note C_, 3
+	note C_, 2
+	note C_, 1
+	note C_, 1
+	note C_, 2
+	octave 3
 	note E_, 2
-	note G_, 2
+	note C_, 1
+	note C#, 1
+	note D_, 3
+	octave 2
+	note D_, 3
+	note D_, 2
+	note D_, 1
+	note D_, 1
+	note D_, 2
+	octave 3
+	note D_, 2
+	octave 2
+	note A_, 2
+	octave 3
+	note C_, 3
+	octave 2
+	note C_, 3
+	note C_, 2
+	note C_, 1
+	note C_, 1
+	note C_, 2
 	endchannel
 
-Music_Gym_branch_be44:
+Music_Gym_Ch2_branch_3:
+	octave 2
+	note D_, 4
 	note D_, 2
-	note F_, 2
+	octave 1
+	note A_, 1
+	note A_, 1
+	note A_, 1
+	octave 2
+	note D_, 1
+	octave 1
+	note A_, 2
+	octave 2
+	note A_, 2
 	note D_, 2
-	note F_, 2
-	note D_, 2
-	note F_, 2
-	note D_, 2
-	note F_, 2
+	note C_, 4
+	note C_, 2
+	octave 1
+	note G_, 1
+	note G_, 1
+	note G_, 1
+	octave 2
+	note C_, 1
+	octave 1
+	note G_, 2
+	octave 2
+	note G_, 2
+	note C_, 2
+	octave 1
+	note B_, 2
+	rest 2
+	note B_, 2
+	note F#, 1
+	note F#, 1
 	endchannel
 
-Music_Gym_branch_be4d:
-	note F_, 2
-	note A#, 2
-	note F_, 2
-	note A#, 2
-	note F_, 2
-	note A#, 2
-	note F_, 2
-	note A#, 2
+Music_Gym_Ch3:
+	vibrato 20, 2, 3
+
+Music_Gym_Ch3_branch_1:
+	notetype 12, 1, 4
+	octave 6
+	note D_, 2
+	octave 4
+	note D_, 1
+	note D_, 1
+	note D_, 1
+	rest 1
+	octave 5
+	note A_, 1
+	octave 6
+	note D_, 1
+	note C_, 2
+	octave 4
+	note C_, 1
+	note C_, 1
+	note C_, 1
+	rest 1
+	octave 5
+	note A_, 1
+	octave 6
+	note C_, 1
+	octave 5
+	note B_, 2
+	octave 3
+	note G_, 1
+	note G_, 1
+	note G_, 1
+	rest 1
+	octave 5
+	note B_, 1
+	note B_, 1
+	note B_, 4
+	octave 6
+	note C#, 4
+	octave 5
+	note D_, 12
+	octave 4
+	note A_, 2
+	octave 5
+	note D_, 2
+	note E_, 1
+	rest 2
+	note C_, 1
+	note C_, 4
+	note G_, 4
+	note E_, 4
+	note D_, 12
+	octave 4
+	note A_, 2
+	octave 5
+	note D_, 2
+	note C_, 1
+	rest 1
+	note C_, 1
+	note D_, 1
+	note E_, 8
+	rest 4
+	note F#, 12
+	note E_, 2
+	note F#, 2
+	note G_, 1
+	rest 2
+	note E_, 1
+	note E_, 4
+	octave 6
+	note C_, 1
+	rest 2
+	octave 5
+	note G_, 1
+	note G_, 4
+	note F#, 12
+	note E_, 2
+	note F#, 2
+	note G_, 2
+	note E_, 1
+	note G_, 1
+	octave 6
+	note C_, 8
+	octave 5
+	note B_, 4
+	notetype 12, 1, 3
+	callchannel Music_Gym_Ch3_branch_2
+	rest 2
+	note A_, 2
+	octave 6
+	note D_, 2
+	octave 5
+	note A_, 2
+	octave 6
+	note C_, 4
+	octave 5
+	note A#, 4
+	note A_, 4
+	note G_, 4
+	callchannel Music_Gym_Ch3_branch_2
+	note A_, 1
+	rest 1
+	note A_, 1
+	note B_, 1
+	octave 6
+	note C_, 4
+	note D_, 1
+	rest 1
+	note D_, 1
+	note E_, 1
+	note F#, 8
+	note E_, 4
+	loopchannel 0, Music_Gym_Ch3_branch_1
+
+Music_Gym_Ch3_branch_2:
+	octave 5
+	note A_, 6
+	octave 6
+	note D_, 6
+	octave 5
+	note A_, 4
+	octave 6
+	note C_, 4
+	octave 5
+	note B_, 4
+	note A_, 4
+	note G_, 4
+	note F#, 1
+	rest 1
+	note F#, 1
+	note G_, 1
+	note A_, 4
 	endchannel
-; 0xbe56
